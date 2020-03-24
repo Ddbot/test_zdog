deaedde4de068a674a550f03fa6ba9db198f3565
Ddbot <rakotoniaina.andry@gmail.com>
2020-03-24 16:06:16 +0100

    Updated clipPath in UKFlag
    to comply with React

--- src/components/ukFlag.js
+++ src/components/ukFlag.js
@@ -7,14 +7,14 @@ const UKFlag = (props) => {
     <clipPath id="a">
       <path d="M0 0h1200v600H0z"/>
     </clipPath>
-    <g clip-path="url(#a)">
+    <g clipPath="url(#a)">
       <path fill="#012169" fillRule="nonzero" d="M0 0h1200v600H0z"/>
       <path d="M0 0l1200 600m0-600L0 600" fillRule="nonzero" stroke="#fff" strokeWidth="120"/>
       <g transform="matrix(2 0 0 2 -600 -300)">
         <clipPath id="b">
           <path d="M600 300h600v300L600 300z"/>
         </clipPath>
-        <g clip-path="url(#b)">
+        <g clipPath="url(#b)">
           <path d="M0 0l1200 600m0-600L0 600" fillRule="nonzero" stroke="#c8102e" strokeWidth="80"/>
         </g>
       </g>
