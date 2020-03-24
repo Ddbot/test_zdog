e9d660e12785a40a40c873f8ccd38ab794dd7a38
Ddbot <rakotoniaina.andry@gmail.com>
2020-03-24 23:39:03 +0100

    Refactor Slide.js

--- src/components/slide.js
+++ src/components/slide.js
@@ -3,15 +3,7 @@ import React from "react";
 import './styles/slide.css';
 
 const Slide = (props) => {
-  
-  // let printMarkup = () => {
-  //   return {
-  //     __html: props.content
-  //   };
-  // };
-
-  return (<div className="slide text" dangerouslySetInnerHTML={{__html: props.content}} role={'document'}/>);
-  
+  return (<div className="slide text" dangerouslySetInnerHTML={{ __html: props.content }} role={'document'} onMouseMove={props.onMouseMove}/>); 
 };
 
 export default Slide;
