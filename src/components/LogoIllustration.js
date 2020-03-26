1b3c7ddcde7eea9bd362d80b12c3f0729b4fef2f
Ddbot <rakotoniaina.andry@gmail.com>
2020-03-26 08:42:01 +0100

    Using useState in LogoIllustration
    pour animer Zdog

--- src/components/LogoIllustration.js
+++ src/components/LogoIllustration.js
@@ -2,28 +2,30 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Illustration, Cone } from 'react-zdog';
 
-let illustrationRef = useRef(null);
-let coneRef = useRef(null);
+import gsap from 'gsap';
 
 const LogoIllustration = (props) => {
 
-    let [x, setX ] = useState(props.rotation.x);
-    let [y, setY] = useState(props.rotation.y);
+    let [rot, setRot ] = useState(null);
+    let [tl] = useState(gsap.timeline({ defaults: { paused: true, duration: 1 }}));
 
-    return (<Illustration ref={ element => {
-        illustrationRef = element;
-    }} zoom={1}>
+    let coneRef = useRef(null);
+
+    useEffect(() => {
+        setRot({ x: props.rotation.x, y: props.rotation.y });
+        tl.to(coneRef, { rotate: rot }).play();
+    }, [props.rotation.x, props.rotation.y, tl]);
+
+    return(<Illustration zoom={1}>
         <Cone
-            ref={ element => {
-                coneRef = element;
-            }}
+            ref={ element => { coneRef = element }}
             diameter={240}
             length={240}
             stroke={false}
             color={'rebeccapurple'}
             backface={'#C25'}
             width={240}
-            rotate = { {x:x, y:y} }
+            rotate = { rot }
         />
   </Illustration>)
 }
