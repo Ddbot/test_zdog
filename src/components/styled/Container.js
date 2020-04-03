9d14d1775e51d5d0197aaca53718fe3dda63d088
Ddbot <rakotoniaina.andry@gmail.com>
2020-04-03 21:33:00 +0200

    Create Container styled-component

---
+++ src/components/styled/Container.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div `
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: center;
+	align-items: center;
+	min-width: 960px;
+	height: 60vh;
+
+	margin-top: 13vh;
+
+	font-size: 2rem;
+	font-family: "Roboto";
+	z-index: -1;
+
+	p {
+		
+		width: 70vw;
+	}
+`;
+
+export default Container;
