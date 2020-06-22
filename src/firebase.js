578c5db6b877ff722e2659735da15f995c8b3a57
Ddbot <rakotoniaina.andry@gmail.com>
2020-06-22 16:22:17 +0200

    Add environment variables in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // My web app's Firebase configuration
 var config = {
-      apiKey: "AIzaSyBnuxr8gtSa_tX1Fs2wk_OfDtoCpVqJ1uk",
+      apiKey: FIREBASE_API_KEY,
       authDomain: "loterie-73336.firebaseapp.com",
       databaseURL: "https://loterie-73336.firebaseio.com",
       projectId: "loterie-73336",
       storageBucket: "loterie-73336.appspot.com",
       messagingSenderId: "362376477806",
-      appId: "1:362376477806:web:9827416aab616e73437a7f",
+      appId: FIREBASE_APP_ID,
       measurementId: "G-CQ96CKCBMS"
   };
   // Initialize Firebase
