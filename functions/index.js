c01dbef8715ddb0537f3fc2116e50cc6fff751ff
Ddbot <rakotoniaina.andry@gmail.com>
2020-06-23 21:45:46 +0200

    Added onCadeauUpdate in functions folder

--- functions/index.js
+++ functions/index.js
@@ -11,3 +11,18 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+export const onCadeauUpdate =
+    functions.firestore.document('cadeaux/{cadeauId}').onUpdate(change => { 
+        const before = change.before.data();
+        const after = change.after.data();
+
+        const payload = {
+            data: {
+                name: String(after.name),
+                quantite: String(after.quantite)
+            }
+        }
+        return admin.messaging().sendToTopic('cadeaux-disponibles', payload)
+            .catch(error => console.log('FCM failed', error));
+    });
